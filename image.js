d73ca3785049fb021ec55e5e877110822a40a2d4
mrignis <143128807+mrignis@users.noreply.github.com>
2024-01-30 04:11:27 +0000

    Deploying to gh-pages from @ mrignis/goit-js-hw-11@e26494d73ed84dd3b674364a864ec5c9177348c7 🚀

--- image.js
+++ image.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '42103820-367af78541649bbd92098b568';
-const searchParams = new URLSearchParams({
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: 'true',
-  per_page: 40,
-});
 
 export default class ImagesApiService {
   constructor() {
@@ -16,12 +10,25 @@ export default class ImagesApiService {
   }
 
   async getImage() {
-    const response = await axios.get(
-      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&${searchParams}&page=${this.page}`
-    );
-    this.page += 1;
+    try {
+      const response = await axios.get(BASE_URL, {
+        params: {
+          key: API_KEY,
+          q: this.searchQuery,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+          per_page: 40,
+          page: this.page,
+        },
+      });
 
-    return response.data;
+      this.page += 1;
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching images:', error);
+      throw error;
+    }
   }
 
   resetPage() {
